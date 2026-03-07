26a110c61533fbd3f4d053e9b67911052b8016b9
Mateus Santos <mateussilvanzg@gmail.com>
2026-03-07 17:05:31 -0300

    feat: add sortedTasks

--- src/features/tasks/components/TaskList/TaskList.tsx
+++ src/features/tasks/components/TaskList/TaskList.tsx
@@ -10,17 +10,36 @@ type TaskListProps = {
 };
 
 export function TaskList({ tasks }: TaskListProps) {
+	const sortedTasks = [...tasks].sort((a, b) => Number(a.completed) - Number(b.completed));
+
 	return (
 		<section className={styles.container}>
 			<HeaderSection title="Tarefas">
 				<Link href="/tasks/new/">
-					<Button variant="default">Adicionar</Button>
+					<Button variant="default">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<circle cx="12" cy="12" r="10" />
+							<path d="M8 12h8" />
+							<path d="M12 8v8" />
+						</svg>
+						Adicionar
+					</Button>
 				</Link>
 			</HeaderSection>
 
 			<h2>Lista</h2>
 			<ul className={styles.list}>
-				{tasks.map((task) => (
+				{sortedTasks.map((task) => (
 					<Task key={task.id} id={task.id} completed={task.completed} name={task.title} />
 				))}
 			</ul>
